b10349b7d497dbf529a687738c9b33d2e37f039a
RGReeTy <be.a.reety@gmail.com>
2020-04-15 14:08:48 +0300

    cr8 controller

---
+++ src/main/java/by.epam.xmlparsing/controller/Controller.java
@@ -0,0 +1,45 @@
+package by.epam.xmlparsing.controller;
+
+import by.epam.xmlparsing.controller.command.ActionFactory;
+import by.epam.xmlparsing.controller.command.Command;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String page = null;
+        ActionFactory factory = ActionFactory.getInstance();
+        Command command = factory.defineCommand(request);
+        page = command.execute(request);
+
+        //DELETE
+        System.out.println("Controller message");
+
+        if (page != null) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(page);
+            dispatcher.forward(request, response);
+        } else {
+//            page = ConfigurationManager.getProperty("path.page.index");
+//            request.getSession().setAttribute("nullPage", command + MessageKey.NULL_PAGE);
+//            response.sendRedirect(request.getContextPath() + page);
+        }
+    }
+}
